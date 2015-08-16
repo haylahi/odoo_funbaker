3016e8e1d586befd9a3b72f63e31edf08532c10f
funbaker <stefan@digitalsense.de>
2015-08-16 22:02:13 +0200

    add openerp file

---
+++ partner_tagsearch/__openerp__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+{
+	'name': 'Recursive Partner Tag Search',
+	'description': 'Search Partner Tags recursively',
+	'author': 'Stefan Becker',
+	'depends': ['base'],
+	'data': ['search.xml']
+}
